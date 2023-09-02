a031b4c88e3ec0f14e1687f83afe8cc90b43eaa0
Kienn Thiett <smoothkt4951@gmail.com>
2023-09-02 14:50:14 +0700

    [style]: max height full scr

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -4,7 +4,7 @@ type props = {
 
 function Container({ children }: props) {
   return (
-    <div className="select-none z-10 min-h-[100vh] flex flex-col w-[90%] mx-auto px-1">
+    <div className="select-none z-10 max-h-[100vh] flex flex-col w-[90%] mx-auto px-1">
       {children}
     </div>
   );
